9ab85bb881eafbd68bc96307b6b78f4e44aa27c0
siddiq <siddiq.aiub@gmail.com>
2022-05-22 01:04:23 +0600

    initialize firebase and deploy

---
+++ src/firebase.init.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCpbVoLIXIOVge3yjPS8iewr6jDugQBDSw",
+  authDomain: "react-exp-cycle-eqp-manufactur.firebaseapp.com",
+  projectId: "react-exp-cycle-eqp-manufactur",
+  storageBucket: "react-exp-cycle-eqp-manufactur.appspot.com",
+  messagingSenderId: "921081619777",
+  appId: "1:921081619777:web:34916e5a5417ee60daadc0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
