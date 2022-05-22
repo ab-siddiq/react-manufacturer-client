777cf23adc9e8a28dfde56c190d13b81f6f56529
siddiq <siddiq.aiub@gmail.com>
2022-05-22 09:25:23 +0600

    add google sign in

--- src/Pages/Login/Login.js
+++ src/Pages/Login/Login.js
@@ -1,8 +1,21 @@
 import React from "react";
+import {useSignInWithGoogle} from 'react-firebase-hooks/auth';
+import auth from "../../firebase.init";
 
 const Login = () => {
+    const [signInWithGoogle, user, loading, error] = useSignInWithGoogle(auth);
+    
+    if(user){
+        console.log('user',user);
+    }
+    if(error){
+        console.log('error',error)
+    }
+    const handleGoogleSignIn = () =>{
+        signInWithGoogle();
+    }
   return (
-    <div className="flex items-center justify-center h-screen">
+    <div className="flex items-center justify-center py-10">
       <div class="card w-96 bg-base-100 shadow-xl ">
         <div class="card-body">
           <h2 class="card-title">Please login!</h2>
@@ -34,9 +47,12 @@ const Login = () => {
               </label>
             </div>
           </form>
+          <div class="card-actions justify-end">
+            <button class="btn  btn-primary btn-block">Login</button>
+          </div>
           <div class="divider">OR</div>
           <div class="card-actions justify-end">
-            <button class="btn btn-outline btn-block">Login</button>
+            <button onClick={handleGoogleSignIn} class="btn btn-outline btn-block">Login With Google</button>
           </div>
         </div>
       </div>
--- src/firebase.init.js
+++ src/firebase.init.js
@@ -1,10 +1,19 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
+import {getAuth} from "firebase/auth";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
+  // apiKey: process.env.REACT_APP_apiKey,
+  // authDomain: process.env.REACT_APP_authDomain,
+  // projectId: process.env.REACT_APP_projectId,
+  // storageBucket: process.env.REACT_APP_storageBucket,
+  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  // appId: process.env.REACT_APP_appId
+  
   apiKey: "AIzaSyCpbVoLIXIOVge3yjPS8iewr6jDugQBDSw",
   authDomain: "react-exp-cycle-eqp-manufactur.firebaseapp.com",
   projectId: "react-exp-cycle-eqp-manufactur",
@@ -15,3 +24,5 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+export default auth;
