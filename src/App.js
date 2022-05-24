0a1d405293a737babbc1e1d8a3fc1071904f2aae
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-05-24 14:58:56 +0600

    add not cound component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import { Route, Routes } from 'react-router-dom';
-import './App.css';
-import Footer from './Footer/Footer';
-import Header from './Header/Header';
-import About from './Pages/About/About';
-import Home from './Pages/Home/Home';
-import Login from './Pages/Login/Login';
-import Products from './Pages/Products/Products';
-import Register from './Pages/Register/Register';
+import { Route, Routes } from "react-router-dom";
+import "./App.css";
+import Footer from "./Footer/Footer";
+import Header from "./Header/Header";
+import About from "./Pages/About/About";
+import Home from "./Pages/Home/Home";
+import Login from "./Pages/Login/Login";
+import NotFound from "./Pages/NotFound/NotFound";
+import Products from "./Pages/Products/Products";
+import Register from "./Pages/Register/Register";
 
 function App() {
   return (
     <div className="">
       <Header></Header>
       <Routes>
-        <Route path='/' element={<Home></Home>}></Route>
-        <Route path='/products' element={<Products></Products>}></Route>
-        <Route path='/about' element={<About></About>}></Route>
-        <Route path='/login' element={<Login></Login>}></Route>
-        <Route path='/register' element={<Register></Register>}></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/products" element={<Products></Products>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/register" element={<Register></Register>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
-     <Footer></Footer>
+      <Footer></Footer>
     </div>
   );
 }
