51c7319bbc6a0f6b43ffa5474850f23e65859775
siddiq <siddiq.aiub@gmail.com>
2022-05-29 22:09:11 +0600

    fix login register design bug

--- src/Pages/Home/Categories.js
+++ src/Pages/Home/Categories.js
@@ -4,7 +4,7 @@ import Category from "./Category";
 const Categories = () => {
   const [categories,setCategories] = useState([]);
   useEffect(()=>{
-    fetch('http://localhost:5000/categories')
+    fetch('https://intense-sierra-65183.herokuapp.com/categories')
     .then(res=>res.json())
     .then(data=>setCategories(data))
   },[])
--- src/Pages/Home/Reviews.js
+++ src/Pages/Home/Reviews.js
@@ -5,7 +5,7 @@ const Reviews = () => {
   const [reviews, setReviews] = useState([]);
   console.log(typeof reviews)
     useEffect(() => {
-      fetch("http://localhost:5000/reviews")
+      fetch("https://intense-sierra-65183.herokuapp.com/reviews")
         .then(res => res.json())
         .then(data => setReviews(data));
     }, [])
