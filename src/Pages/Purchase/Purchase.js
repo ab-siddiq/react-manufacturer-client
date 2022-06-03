a7c61f092c25c26ce51c776e4619675f01a324ab
Abu Bakkar Siddiq <abu.bakkar@sebpo.com>
2022-06-03 16:24:41 +0600

    product quantity inc dec

--- src/Pages/Purchase/Purchase.js
+++ src/Pages/Purchase/Purchase.js
@@ -5,8 +5,28 @@ const Purchase = () => {
   const { purchaseId } = useParams();
   const [purchase, setPurchase] = useState({});
   const { name, description, price, quantity, ratings, image } = purchase;
+  const [productQuantity, setProductQuantity] = useState(4);
+  const [error,setError] = useState('');
+  const [errorBG,setErrorBG] = useState('');
+
+
+  const handleDecrease = () => {
+    let error = '';
+    if(productQuantity>4){
+      setProductQuantity(productQuantity-1)
+    }else{
+      setError('need to buy more than 4 product');
+      setErrorBG("ml-[40px] bg-red-500  p-[10px] rounded text-red-200");
+    }
+  };
+  const handleIncrease = () => {
+      setProductQuantity(productQuantity+1);
+      setError('');
+      setErrorBG('');
+  };
+
   useEffect(() => {
-    fetch(`http://localhost:5000/purchase/${purchaseId}`)
+    fetch(`https://intense-sierra-65183.herokuapp.com/purchase/${purchaseId}`)
       .then((res) => res.json())
       .then((data) => setPurchase(data));
   }, []);
@@ -23,14 +43,18 @@ const Purchase = () => {
           </span>
 
           <p className="py-6">{description}</p>
-          <div className="ml-[40px]">
-            <button className="btn">-</button>
-            <input
-              type="text"
-              value={4}
-              className="input input-bordered w-20 max-w-xs m-[20px] text-center"
-            />
-            <button className="btn ">+</button>
+          <span className={`${errorBG}`}>{error}</span>
+          
+          <div className="ml-[40px] flex  items-center">
+            <button className="btn disabled" onClick={handleDecrease}>
+              -
+            </button>
+            <div className="input input-bordered w-20 max-w-xs m-[20px] flex items-center justify-center">
+              {productQuantity}
+            </div>
+            <button className="btn " onClick={handleIncrease}>
+              +
+            </button>
           </div>
           <button className="btn btn-primary mx-[10px]">Buy Now</button>
           <button className="btn btn-primary mx-[10px]">Add To Cart</button>
