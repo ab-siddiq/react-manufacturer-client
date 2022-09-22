15b0ea93d781392ac3b1d5db6263963a1fc4a34b
siddiq <siddiq.aiub2gmail.com>
2022-09-22 11:09:00 +0600

    change heroku repo

--- src/Pages/DashBoard/DashBoardPanel/DashboardPanel.js
+++ src/Pages/DashBoard/DashBoardPanel/DashboardPanel.js
@@ -1,7 +1,10 @@
 import React from "react";
+import useProducts from "../../../hooks/useProducts";
 
 const DashboardPanel = () => {
-  return <div>/dashboard panel</div>;
+  const [products] = useProducts();
+  // return <div>dd</div>
+  return <div>{products.map(product=>product.name)}</div>;
 };
 
 export default DashboardPanel;
